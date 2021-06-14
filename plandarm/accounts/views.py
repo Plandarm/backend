dc02b5477875041e6f5d03cec96ce03636054fe8
HrabCOrp <oxcherson@gmail.com>
2021-06-14 11:29:03 +0300

    Log in/Log out functionality
    Added: templates and styles
     - index.html: main page placeholder
     - login-page.html: login form (modified import from frontend-prototype)
     - login-page.css: styles for login form (import from frontend-prototype)
    
    Added: url patterns for main page, login, logout
    Added: views for login and main page
    Added: log in/log out functionality for login/logout views
    
    Changed: login-page.html updated to enable log in functionality
    
    Removed: redundant </input> closing tags from login-page.html

--- plandarm/accounts/views.py
+++ plandarm/accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+
+def mainPage(request):
+    return render(request, 'accounts/index.html')
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('/admin')
+        else:
+            messages.info(request, 'Incorrect login data')
+
+    return render(request, 'accounts/login-page.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
