0b5737a30d0ee90c9697b9fd3ffc9d352f0017c7
Rostyslav Sviatoshchuk <svyatoschuk@gmail.com>
2021-06-14 19:58:46 +0300

    Default page creation for new user

--- plandarm/pages/models.py
+++ plandarm/pages/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 from accounts.models import Profile
-# Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class Page(models.Model):
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
-    title = models.CharField(max_length=200, null=True) 
-    html = models.TextField(null=True)
+    title = models.CharField(max_length=200, default="New Page") 
+    html = models.TextField(default='<p class="block text-block"></p>')
 
     create_date = models.DateTimeField(auto_now_add=True, null=True)
     change_date = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
-        return self.title
+        return f"{self.owner.user.username}:{self.id}:{self.title}"
+
+
+@receiver(post_save, sender=Profile)
+def create_default_page(sender, instance, created, **kwargs):
+    if created:
+        Page.objects.create(owner=instance)
