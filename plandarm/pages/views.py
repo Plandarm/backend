acbe06e7d7c1796d8f86cda8adc16423c5575274
HrabCOrp <oxcherson@gmail.com>
2021-06-15 13:33:28 +0300

    Page viewing permissions (fix)
    Changed: pages can be viewed only by their respective owners
    Changed: redirections
      - site root redirects to login, if not logged in
      - site root redirects to the first page of the user, if logged in
    
      - login page redirects to the first page of the user, on successful log in
      - login page redirects to the first page of the user, if logged in
    
      - registration page redirects to login page on successful registration
      - registration page redirects to first page of the user, if logged in

--- plandarm/pages/views.py
+++ plandarm/pages/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from pages.models import Page 
+from django.http import HttpResponse
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='login')
 def page(request, page_id):
     page = Page.objects.get(id=page_id)
     context = {'page': page}
-    
+ 
+    if page.owner.user.id != request.user.id:
+        return HttpResponse('You are not authorized to view this page')
+
     return render(request, 'pages/editor.html', context)
