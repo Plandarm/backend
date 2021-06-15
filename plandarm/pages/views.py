3872d7d482a2647167754f04e24a34b69272cb10
Rostyslav Sviatoshchuk <svyatoschuk@gmail.com>
2021-06-15 13:50:28 +0300

    CRUD backend for pages
    Added: support for creation of new page
    Added: support for saving page
    Added: support for deletion of page
    
    Fixed: __str__ for Page model

--- plandarm/pages/views.py
+++ plandarm/pages/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render
-from pages.models import Page 
+from django.shortcuts import render, redirect
+from pages.models import Page
+from django.http import HttpResponse
+import json
 
-# Create your views here.
 
 def page(request, page_id):
     page = Page.objects.get(id=page_id)
     context = {'page': page}
     
     return render(request, 'pages/editor.html', context)
+
+
+def createPage(request):
+    current_user = request.user
+    page = Page.objects.create()
+    page.owner = current_user.profile
+    page.save()
+    return redirect(f'/page/{page.id}/')
+
+
+def savePage(request, page_id):
+    if request.method == 'POST':
+        dict_request = json.loads(request.body)
+        page = Page.objects.get(id=page_id)
+        page.title = dict_request['title']
+        page.html = dict_request['body']
+        page.save()
+        return HttpResponse(status=200)
+    else:
+        return HttpResponse(status=405)
+
+
+def deletePage(request, page_id):
+    page = Page.objects.get(id=page_id)
+    page.delete()
+    return HttpResponse("Page was deleted", status=200)
