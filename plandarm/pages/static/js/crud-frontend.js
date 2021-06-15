68a320cdfd1cab3481f0dc1e5f2edb5da21c543d
Buhtiy Oleksandr <sasha007.h@gmail.com>
2021-06-15 12:28:15 +0300

    fixed request

---
+++ plandarm/pages/static/js/crud-frontend.js
@@ -0,0 +1,29 @@
+async function savePage() {
+    main  = document.querySelector("main.page-main");
+
+    title = main.querySelector("section.main-heading").childNodes[1].innerHTML;
+    body  = main.querySelector("section.main-body").innerHTML;
+    csrftoken = getCookie("csrftoken");
+
+    let requestJSON = {
+        title: title,
+        body: body,
+    };
+
+    href = window.location.href.split("/")
+
+    let response = await fetch(`/page/${href[href.length - 2]}/save`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json;charset=utf-8",
+            "X-CSRFToken": csrftoken,
+        },
+        body: JSON.stringify(requestJSON),
+      });
+}
+
+function getCookie (name) {
+    let value = '; ' + document.cookie;
+    let parts = value.split(`; ${name}=`);
+    if (parts.length == 2) return parts.pop().split(';').shift();
+}
