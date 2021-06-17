8f84efff5664bf1ed81a39e333a9f4144ee94e1c
Buhtiy Oleksandr <sasha007.h@gmail.com>
2021-06-17 21:39:52 +0300

    fixed chrome default enter key behaviour

--- plandarm/pages/static/js/add-remove-blocks.js
+++ plandarm/pages/static/js/add-remove-blocks.js
@@ -3,31 +3,40 @@ const workspace = document.querySelector("section.main-body");
 document.addEventListener("keydown", event => shortcut(event))
 function shortcut(event) {
     code = event.code;
-    if (code === "Enter" && event.shiftKey) {
+    if (code === "Enter" && document.activeElement.parentNode.className === "main-heading") {
+        event.preventDefault();
+    } else if (code === "Enter" && !event.shiftKey) { //Prevents excessive div spawn
+        event.preventDefault();
+        //Time spent: 3h to get it right-ish (5 lines) ಠ_ಠ
+        let range = window.getSelection().getRangeAt(0);
+        let linebreak = document.createElement("br");
+        range.deleteContents()
+        range.insertNode(linebreak)
+        range.collapse()
+
+    } else if (code === "Enter" && event.shiftKey) {
         event.preventDefault();
         addBlock(event);
         blocks = updateBlocks();
-    }
-    else if (code === "Delete" && event.ctrlKey) {       
+    } else if (code === "Delete" && event.ctrlKey) {       
         event.preventDefault(); 
         deleteBlock(event);
         blocks = updateBlocks();
     }
 }
 
-//Works like stack
 function addBlock(event) { //Shift + Enter = New block after focused
     console.log("Add");
 
-    var textBlock = document.createTextNode("");
-    var domElement = document.createElement("p");
+    let textBlock = document.createTextNode("");
+    let domElement = document.createElement("p");
     domElement.appendChild(textBlock);
     
     domElement.setAttribute("class", "text-block block");
     domElement.setAttribute("contenteditable", "true");
     
     if (document.activeElement.parentElement === workspace) {
-        var focusedElement = document.activeElement;
+        let focusedElement = document.activeElement;
         insertAfter(domElement, focusedElement);
         domElement.focus();
     }
@@ -38,13 +47,12 @@ function deleteBlock(event) { //Delete = remove focused block
     console.log("Delete")
 
     if (document.activeElement.parentElement === workspace) {
-        var focusedElement = document.activeElement;
-        var previous = focusedElement.previousElementSibling;
+        let focusedElement = document.activeElement;
+        let previous = focusedElement.previousElementSibling;
         focusedElement.remove();
         previous.focus();
     }
 }
-
 
 //For insertion
 function insertAfter(el, referenceNode) {
